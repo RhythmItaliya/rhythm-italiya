2a757e06a74675beadb68c12a23409611b557686
rhythmitaliya <ritaliya2003@gmail.com>
2024-09-26 10:19:35 +0530

    26-09-2024

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,33 +1,30 @@
 import "./globals.css";
 import ScrollAnim from "@/Components/Ui/ScrollAnim/ScrollAnim";
-import Head from "next/head";
+
 export const metadata = {
   title: "Rhythm Italiya",
   description: "Rhythm Italiya - Full Stack Developer specializing in modern web technologies and creating dynamic, user-friendly applications.",
+  url: "https://rhythmitaliya.com",
+  image: "https://res.cloudinary.com/ds9ufpxom/image/upload/v1727323304/Img/nruph7xbowtmlxslzacv.png",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
 
-      <Head>
-        {/* Twitter Meta Tags */}
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:url" content={metadata.url} />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={metadata.title} />
         <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content="/images/og.png" />
-
-        {/* Open Graph Meta Tags */}
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content="/images/og.png" />
-        <meta property="og:site_name" content="Rhythm Italiya Portfolio" />
-        <meta property="og:url" content="https://rhythmitaliya.com" />
-
-        {/* General Meta Tags */}
-        <meta name="description" content={metadata.description} />
-        <title>{metadata.title}</title>
-      </Head>
+        <meta name="twitter:image" content={metadata.image} />
+      </head>
 
       <body className="min-h-screen bg-[#082231]">
         <ScrollAnim />
