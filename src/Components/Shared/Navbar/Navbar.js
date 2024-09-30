66439f4d2c5ddd3be4bc678e0c5a8e185264ab76
rhythmitaliya <ritaliya2003@gmail.com>
2024-09-30 09:21:01 +0530

    30-09-2024

--- src/Components/Shared/Navbar/Navbar.js
+++ src/Components/Shared/Navbar/Navbar.js
@@ -2,7 +2,7 @@
 import { Link } from "react-scroll";
 import Image from "next/image";
 import Sticky from "react-stickynode";
-import Logo from '@/images/logo.png'
+import Logo from '@/images/logo.png';
 import { HiOutlineMenu } from "react-icons/hi";
 import { useState } from "react";
 import { Drawer } from 'antd';
@@ -26,7 +26,7 @@ const Navbar = () => {
                             duration={500}
                         >
                             <div className="flex items-center">
-                                <Image priority={false} className="h-10 w-auto md:h-auto lg:h-14" src={Logo} alt="logo" />
+                                <Image priority={false} className="h-8 w-auto sm:h-10 md:h-12 lg:h-14 px-4" src={Logo} alt="logo" />
                             </div>
                         </Link>
                         <div className="hidden md:block">
@@ -49,10 +49,14 @@ const Navbar = () => {
                             </ul>
                         </div>
                         <div className="flex items-center gap-x-3">
-                            <a href={aboutMe.socialLinks.github} target="blank" className="relative items-center justify-start inline-block px-5 py-3 overflow-hidden font-bold rounded-full group">
-                                <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-[#00A6E8] opacity-[3%]"></span>
-                                <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-[#00A6E8] opacity-100 group-hover:-translate-x-8"></span>
-                                <span className="relative w-full text-left text-[#00A6E8] transition-colors duration-200 ease-in-out group-hover:text-white">Github Profile</span>
+                            <a
+                                href={aboutMe.socialLinks.github}
+                                target="blank"
+                                className="relative inline-block px-4 py-2 sm:px-5 sm:py-3 font-bold text-sm sm:text-base md:text-lg rounded-full group"
+                            >
+                                <span className="relative text-[#00A6E8] transition-colors duration-200 ease-in-out group-hover:text-white">
+                                    Github Profile
+                                </span>
                                 <span className="absolute inset-0 border-2 border-[#00A6E8] rounded-full"></span>
                             </a>
                             <HiOutlineMenu onClick={() => setOpen(true)} className="text-3xl text-gray-200 mr-3 md:hidden" />
